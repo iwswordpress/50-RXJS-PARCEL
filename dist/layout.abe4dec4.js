af102b6438e2ca8fd41c96780f908987e0dbf029
iwswordpress <iwswordpress@gmail.com>
2021-06-20 09:30:23 +0100

    REGISTER in root

--- dist/layout.abe4dec4.js
+++ dist/layout.abe4dec4.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55429" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54569" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
